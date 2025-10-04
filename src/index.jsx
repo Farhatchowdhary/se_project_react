9bef590509b9c376e8bc2585977f8e5f6a6552bf
Farra <farhatchoudhary2@gmail.com>
2025-10-03 23:18:19 -0400

    sprint11 submission

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx"; 
-import "./Content.css";
-import "./Dashboard.css";
-import "./Logo.css";
-import "./Reviews.css";
+
 
 // Render the App inside the root div
 ReactDOM.createRoot(document.getElementById("root")).render(
