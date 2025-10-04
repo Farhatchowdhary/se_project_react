ad3e1ffdb60da432fc6203c766f395d196538bc5
Farra <farhatchoudhary2@gmail.com>
2025-10-03 23:15:09 -0400

    sprint 11 first try

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx"; // Make sure App.jsx exists
+import App from "./components/App.jsx"; 
+import "./Content.css";
+import "./Dashboard.css";
+import "./Logo.css";
+import "./Reviews.css";
 
 // Render the App inside the root div
 ReactDOM.createRoot(document.getElementById("root")).render(
