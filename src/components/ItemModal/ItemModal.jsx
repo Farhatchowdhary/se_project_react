a901a1ea7df36d8ab36412d315294eae6a9a0031
Farra <farhatchoudhary2@gmail.com>
2025-10-03 23:00:58 -0400

    sprint11 submission first try

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -7,22 +7,24 @@ const ItemModal = ({ isOpen, onClose, card, onDelete }) => {
         <div className={`modal ${isOpen ? 'modal_is-opened' : ''}`} onClick={onClose}>
             <div className="modal__content" onClick={e => e.stopPropagation()}>
                 <button className="modal__close" onClick={onClose}></button>
-                
+
                 {card && (
                     <div className="modal__image-wrapper">
                         <img src={card.link} alt={card.name} className="modal__image" />
-                        <button className="modal__delete-btn" onClick={(e)  => {
-                            console.log("Deletebutton clicked!");
+                        <button 
+                        className="modal__delete-btn"
+                         onClick={(e) => {
+                            console.log("Delete button clicked!");
                             e.preventDefault();
                             e.stopPropagation();
                             console.log("About to call onDelete");
                             onDelete();
-                        }}
+                            }}
                         >
                             Delete item
-                            </button>
+                        </button>
                         <h2 className="modal__title">{card.name}</h2>
-                        
+
                         <p className="modal__description">{card.description}</p>
                         {card.weather && <p className="modal-weather__name">Weather: {card.weather}</p>}
                     </div>
