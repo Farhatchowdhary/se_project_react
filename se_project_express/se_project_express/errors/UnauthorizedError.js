80b20fb9f9d7bf9d62acaa854db86ec4673f2913
Farhat Choudhary <your_email@example.com>
2025-11-06 11:55:34 -0500

    Remove duplicate files

--- se_project_express/se_project_express/errors/UnauthorizedError.js
+++
@@ -1,6 +0,0 @@
-export class UnauthorizedError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 401;
-  }
-}
--- se_project_express/se_project_express/utils/config.js
+++
@@ -1,2 +0,0 @@
-const JWT_SECRET = "my-project-secret-2025";
-export { JWT_SECRET };
